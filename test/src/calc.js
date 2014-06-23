8bb47f56374434700df5f15c8c881776e4f54e7d
Sivanandan <vsivanandan@LM-SJC-00713138.(none)>
2014-06-22 23:57:31 -0700

    removed

--- test/src/calc.js
+++
@@ -1,8 +0,0 @@
-function calc(yourName) {
-	this._yourName = yourName;    
-};
-
-calc.prototype.add = function(firstOperand, secondOperand) {
-	var result = firstOperand + secondOperand;
-	return this._yourName + " the addition result is: " + result;
-}
